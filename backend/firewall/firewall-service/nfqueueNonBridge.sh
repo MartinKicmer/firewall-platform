fe256a3a3a76bbbdfae9b2a366a50e8e2558d6ba
Andrew <ondra.mic2@gmail.com>
2026-02-11 21:18:11 +0100

    Better script for excluding loopback

--- backend/firewall/firewall-service/nfqueueNonBridge.sh
+++ backend/firewall/firewall-service/nfqueueNonBridge.sh
@@ -8,9 +8,9 @@ sudo nft add chain inet fw_test input { type filter hook input priority 0 \; }
 sudo nft add chain inet fw_test forward { type filter hook forward priority 0 \; }
 sudo nft add chain inet fw_test output { type filter hook output priority 0 \; }
 
-sudo nft add rule inet fw_test input counter queue num 0
+sudo nft add rule inet fw_test input iifname != "lo" counter queue num 0
 sudo nft add rule inet fw_test forward counter queue num 0
-sudo nft add rule inet fw_test output counter queue num 0
+sudo nft add rule inet fw_test output oifname != "lo" counter queue num 0
 
-echo "[*] Rules applied (In/Forward/Out). Current state:"
+echo "[*] Rules applied. Localhost (lo) is EXCLUDED from filtering."
 sudo nft list table inet fw_test
