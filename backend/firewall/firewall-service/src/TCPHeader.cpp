44d233ca41fde0edc0592848469ae55b794cce85
Ondresss <ondra.mic2@gmail.com>
2026-04-22 10:12:01 -0500

    TCP header

--- backend/firewall/firewall-service/src/TCPHeader.cpp
+++ backend/firewall/firewall-service/src/TCPHeader.cpp
@@ -6,32 +6,37 @@
 #include <stdexcept>
 
 void TCPHeader::parse() {
- 
-    if(this->payloadSize <= 0) throw std::runtime_error("Payload too small for TCP header\n");
+    if (this->payloadSize < sizeof(struct tcphdr)) {
+        throw std::runtime_error("Payload too small for basic TCP header (min 20B required)\n");
+    }
+
     const uint8_t* data = this->payloadData;
     auto tcpHeader = reinterpret_cast<const struct tcphdr*>(data);
 
+    uint8_t headerLength = tcpHeader->doff * 4;
 
-    this->sPort = ntohs(tcpHeader->source);
-    this->dPort = ntohs(tcpHeader->dest);
-    this->seqNumber  = ntohl(tcpHeader->seq);    
+    if (this->payloadSize < headerLength) {
+        std::cerr << "[TCP Error] Payload size: " << this->payloadSize
+                  << ", Header indicates: " << headerLength << " bytes" << std::endl;
+        throw std::runtime_error("Payload smaller than TCP header offset indicates (Options truncated?)\n");
+    }
+
+    this->sPort      = ntohs(tcpHeader->source);
+    this->dPort      = ntohs(tcpHeader->dest);
+    this->seqNumber  = ntohl(tcpHeader->seq);
     this->ackNumber  = ntohl(tcpHeader->ack_seq);
     this->windowSize = ntohs(tcpHeader->window);
     this->checkSum   = ntohs(tcpHeader->check);
     this->urgPointer = ntohs(tcpHeader->urg_ptr);
-    this->flags = data[13];
-    uint8_t headerLength = tcpHeader->doff * 4;
+    this->flags      = data[13];
 
-    if (this->payloadSize < headerLength) {
-        throw std::runtime_error("Payload smaller than TCP header offset indicates\n");
+    const uint8_t* nextPayload = data + headerLength;
+    std::size_t nextPayloadSize = this->payloadSize - headerLength;
+
+    if (nextPayloadSize > 0) {
+        this->parseNext(nextPayload, nextPayloadSize);
     }
-    const uint8_t* nextPayload = data + sizeof(struct tcphdr);
-    std::size_t nextPayloadSize = this->payloadSize - sizeof(struct tcphdr);
-
-    this->parseNext(nextPayload, nextPayloadSize);
-
 }
-
 nlohmann::json  TCPHeader::serialize() const {
     return nlohmann::json{
         {"source_port", sPort},
